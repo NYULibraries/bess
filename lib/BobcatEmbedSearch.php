32cb209a30704d9f062732bd7bbe0e1e4aea8b36
Barnaby Alter <barnaby.alter@nyu.edu>
2017-02-27 16:09:04 -0500

    Done debugging

--- lib/BobcatEmbedSearch.php
+++ lib/BobcatEmbedSearch.php
@@ -10,7 +10,7 @@ require_once('BobcatEmbed.php');
  */
 class Bobcat_Embed_Search extends BobCat_Embed
 {
-	const DEBUG = true;
+	const DEBUG = false;
   /**
    * Upon submission of the form, generate the correct url to redirect to
    */
